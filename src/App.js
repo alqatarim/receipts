6ec51c09bf33278ef610b6e7d6eaf9975ba19285
Moh <m.alqatari@gmail.com>
2024-03-19 20:29:09 +0300

    Add cors package and update API endpoint

--- src/App.js
+++ src/App.js
@@ -10,33 +10,6 @@ function App() {
     
 };
 
-// import ReceiptDetails from './ReceiptDetails';
-// // export default App;
-// import React, { useState, useEffect } from 'react';
 
-// const App = () => {
-//   const [receipts, setReceipts] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch customer receipts from the backend when the component mounts
-//     fetch('/api/receipts')
-//       .then(response => response.json())
-//       .then(data => setReceipts(data))
-//       .catch(error => console.error('Error fetching receipts:', error));
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Customer Receipts</h1>
-//       <ul>
-//         {receipts.map(receipt => (
-//           <li key={receipt.id}>
-//             Customer: {receipt.customerName} - Amount: {receipt.amount}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
 
  export default App;
--- src/ReceiptDetails.js
+++ src/ReceiptDetails.js
@@ -1,29 +1,39 @@
 import React, { useState, useEffect } from 'react';
 
 const ReceiptDetails = () => {
-
+  var responseClone;
   const [receipts, setReceipts] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(true);
 
-    fetch('/api/receipts')
-      .then(response => response.json()) 
+    fetch('http://localhost:5001/api/receipts')
+    .then(function (response) {
+      responseClone = response.clone(); // 2
+      return response.json();
+    })
       .then(data => {
+        console.log(data); 
         setReceipts(data);
       })
       .catch(error => {
-        console.error('Error fetching receipts', error);
+        console.error('Error fetching receipts', error, responseClone);
       })
       .finally(() => {
         setLoading(false);  
       });
+  }, []);
 
-  }, []); // Remove 'receipts' from the dependency array
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <p>Loading...</p>
+      </div>
+    );
+  }
 
-  if (loading) return <p>Loading...</p>;
-
+  
   return (
     <div>
       <h1>Customer Receipts</h1>
